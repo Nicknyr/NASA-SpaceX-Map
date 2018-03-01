d05efa5c5d9ea2f5263d33774a8282d7a5bc8c2a
Nick Kinlen <nick_kinlen@hotmail.com>
2018-03-01 12:10:25 -0500

    Got map circles rendering from SO

--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { render } from 'react-dom';
 import { Map, Marker, Popup, TileLayer, Tooltip, Circle } from 'react-leaflet';
 import DivIcon from 'react-leaflet-div-icon';
@@ -33,6 +33,7 @@ export default class MapView extends React.Component {
 
 
 render() {
+
    const position = [40.730610, -73.935242];
    const x = 40.730610;
    const y = -73.935242;
@@ -46,7 +47,7 @@ render() {
             attribution="<attribution>" />
             <Circle
               center={[x, y]}
-              radius={7}
+              radius={30000}
               color={"red"}>
             </Circle>
           <Marker position={position}>
@@ -59,5 +60,79 @@ render() {
         </Map>
     );
   }
-
 }
+*/
+
+
+
+import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Tooltip,Popup, Circle} from 'react-leaflet';
+import axios from 'axios';
+
+  const url = 'https://api.spacexdata.com/v2/launchpads';
+  const leafURL = "https://api.mapbox.com/styles/v1/nicknyr/cje7mtk2y6gf92snsydobiahf/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoibmlja255ciIsImEiOiJjajduNGptZWQxZml2MndvNjk4eGtwbDRkIn0.L0aWwfHlFJVGa-WOj7EHaA";
+
+  class App extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        latlng: {
+        lat: 28.5618571,
+        lng: -80.577366,
+        },
+        data: []
+      }
+    }
+
+  componentWillMount() {
+    axios.get(url).then(res => {
+    this.setState({data: res.data})
+    }).catch(err => {
+      console.log('error')
+    })
+  }
+
+  render() {
+    const {data} = this.state;
+    console.log(data);
+
+    return (
+      <div>
+        <Map
+          style={{height: "100vh"}}
+          center={this.state.latlng}
+          zoom={4}>
+        <TileLayer
+          url={leafURL}
+          attribution="<attribution>" />
+
+          {data.map((elem, i) => {
+            return (
+              <Marker
+                key={i}
+                position={{lat:elem.location.latitude, lng: elem.location.longitude}}>
+              <Popup>
+                <span>
+                  {elem.full_name}<br />
+                  {elem.status}<br />
+                  {elem.details}<br />
+                  {elem.vehicles_launched.map((elem, i) => {
+                    return ( <p key={i}>{elem}</p>)
+                  })}
+                </span>
+
+                </Popup>
+                  <Circle
+                    center={{lat:elem.location.latitude, lng: elem.location.longitude}}
+                    fillColor="blue"
+                    radius={10000}/>
+                </Marker>
+                )
+              })}
+            </Map>
+          </div>
+        );
+      }
+    }
+
+    export default App;
